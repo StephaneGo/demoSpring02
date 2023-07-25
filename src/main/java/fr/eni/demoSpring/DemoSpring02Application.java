d6f128212a4ade5b3d2bf66138abb2da738366a8
Stephane <sgobin@campus-eni.fr>
2023-07-25 10:23:57 +0200

    version initiale

---
+++ src/main/java/fr/eni/demoSpring/DemoSpring02Application.java
@@ -0,0 +1,13 @@
+package fr.eni.demoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpring02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpring02Application.class, args);
+	}
+
+}
